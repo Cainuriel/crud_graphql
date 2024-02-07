705e0e33a6d8ba60921d94f26df2a04bd86551bf
Fernando Lopez <cainuriel@gmail.com>
2024-02-07 20:03:26 +0100

    debugShowCheckedModeBanner: false,

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
     return GraphQLProvider(
       client: client,
       child: MaterialApp(
-        
+        debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.blue,
